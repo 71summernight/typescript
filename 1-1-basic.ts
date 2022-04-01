d4b966092e992d2206525ad22c0b92b13190cf4d
hyunjinkim <71summernight@naver.com>
2022-04-01 16:30:33 +0900

    abstraction

--- 1-1-basic.ts
+++ 1-1-basic.ts
@@ -160,8 +160,14 @@ const result=jsStrFunc();
 console.log((result as string).length)
 console.log((<string>result).length)
 const wrong: any =5;
-console.log((wrong as Array<number>).push(1));
+//console.log((wrong as Array<number>).push(1));
 
 
+function findNumbers() : number[] | undefined{
+    return undefined
+}
+
+const numbers = findNumbers();
+numbers!.push(2)
 
 }
--- 3-OOP/3-3-encapsulation.ts
+++ 3-OOP/3-3-encapsulation.ts
@@ -3,57 +3,75 @@
         shots:number;
         hasMilk: boolean
     }
-    class CoffeeMaker{
+    interface CoffeeMaker{
+        makeCoffee(shots:number):CoffeeCup
+    }
+    interface CommercialCoffeeMaker {
+        makeCoffee(shots:number): CoffeeCup
+        fillCoffeeBeans(beansL :number):void
+        clean():void
+    }
+
+    class CoffeeMachine implements CoffeeMaker,CommercialCoffeeMaker{
         private static BEANS_GRAM_PER_SHOT:number =7  //class level로 지정됌 
          private coffeeBeansGram: number = 0;
          constructor(coffeeBeans:number){
             this.coffeeBeansGram = coffeeBeans
          }
-         fillCoffeeBeans(beans:number){
+         static makeMachine(coffeeBeans: number):CoffeeMachine{
+             return new CoffeeMachine(coffeeBeans)
+         }
+
+          fillCoffeeBeans(beans:number){
             if(beans < 0){
                 throw new Error('value for beans should be greater than 0')
             }
             this.coffeeBeansGram +=beans
          }
+         clean(): void {
+             console.log('cleaning the CoffeeMachine....')
+         }
+         private grindBeans(shots){
+             console.log( `grinding beans for ${shots}`);
+             if(this.coffeeBeansGram < shots * CoffeeMachine.BEANS_GRAM_PER_SHOT){
+                throw new Error (`Not enough coffee beans!`)
+             }
+             this.coffeeBeansGram -= shots * CoffeeMachine.BEANS_GRAM_PER_SHOT
+         }
+         private preheat(): void{
+             console.log('heating up...')
+         }
+
+         private extract(shots: number):CoffeeCup{
+             console.log(`pulling ${shots}....shots...`)
+             return {
+                 shots,
+                 hasMilk:false,
+             }
+            }
+
          makeCoffee(shots: number):CoffeeCup {
-            if(this.coffeeBeansGram<shots * CoffeeMaker.BEANS_GRAM_PER_SHOT){
-                throw new Error('Not enough coffee beans!');
-            }
-            this.coffeeBeansGram -=shots * CoffeeMaker.BEANS_GRAM_PER_SHOT
-            return {
-                shots,
-                hasMilk: false
-            }
+           this.grindBeans(shots);
+           this.preheat();
+           return this.extract(shots)
+            // if(this.coffeeBeansGram<shots * CoffeeMachine.BEANS_GRAM_PER_SHOT){
+            //     throw new Error('Not enough coffee beans!');
+            // }
+            // this.coffeeBeansGram -=shots * CoffeeMachine.BEANS_GRAM_PER_SHOT
+            // return {
+            //     shots,
+            //     hasMilk: false
+            // }
         }
     }
-    const maker = new CoffeeMaker(32)
+    const maker :CoffeeMachine= CoffeeMachine.makeMachine(32)
     maker.fillCoffeeBeans(30)
     maker.fillCoffeeBeans(20)
     console.log(maker)    
+    const maker2 :CommercialCoffeeMaker= new CoffeeMachine(32)
+    maker2.fillCoffeeBeans(30)
+    maker2.makeCoffee(2)
+    maker2.clean()
+    console.log(maker2)    
 
-    class User{
-        firstName: string;
-        lastName: string;
-        get fullName():string{
-           return`${this.firstName} ${this.lastName}` 
-        }
-        private internalAge = 4;
-        get age(): number{
-            return this.internalAge
-        }
-        set age(num:number){
-            if(num<0){
-                throw new Error('should upper than 0')
-            }
-            this.internalAge = num
-        }
-        constructor(firstName:string, lastName:string){
-            this.firstName = firstName;
-            this.lastName = lastName;
-        }
-    }
-    const user = new User('Steve','Jobs')
-    console.log(user.fullName)
-    user.firstName = 'hyunjin'
-    console.log(user.fullName)
 }
